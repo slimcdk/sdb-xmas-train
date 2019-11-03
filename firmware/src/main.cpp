623918c2e91dc62100b9276b327b448ae742406a
Christian <cs@slimc.dk>
2019-11-03 03:09:32 +0100

    simpler logic

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,133 +1,131 @@
 #include <Arduino.h>
 
 
-// MOTOR IO
-#define MOTOR_SENSE_PIN           2
-#define MOTOR_CTRL_PIN 		        3
-#define MOTOR_DIR_PIN 		        4
-#define TRAIN_MODE_SW_PIN         11
-#define RPI_ACTIVATE_MUSIC_PIN    10
+// IO PIN CONFIGURATION
+#define MOTOR_PULSE_PIN   2
+#define MOTOR_PWM_PIN 		3
+#define MOTOR_DIR_PIN 		4
+#define TRAIN_MODE_PIN    11
 
 
-// train durations in seconds
-//#define TRAIN_LAP_TIME 		      30
-#define TRAIN_RUN_DURATION 	      10000
-#define TRAIN_DURATION	  	      20000
-#define TRAIN_MOTOR_BOOT_TIME     2500
-#define TRAIN_MOTOR_CHECK_UPSTART 2000
+/** TRAIN TIMING SCHEME
+ * |-- boot --|-- run --|-- break --|-- stop --|
+ * |-- run progress  ------------------------- |
+ * |-- motor on time -------------- |
+**/
+#define TRAIN_T_BOOT      2000
+#define TRAIN_T_RUN       36000
+#define TRAIN_T_BREAK     1000
+#define TRAIN_T_STOP      600000
+
+#define TRAIN_S_BOOT      1   
+#define TRAIN_S_MAX		    0.5
+#define TRAIN_S_MIN		    0
+
+#define MOTOR_SAMPLES     30
+#define MOTOR_SAMPLE_TOL  5
 
 
-#define TRAIN_BOOT_SPEED          255   // 100% duty cycle
-#define TRAIN_MAX_SPEED		        127 	// 50% duty cycle
-#define TRAIN_MIN_SPEED		        0	    // 0% duty cycle
 
-#define TRAIN_MOTOR_SAMPLES       30
-
-
+bool is_motor_running, run_train, boot_mode, normal_mode, breaking_mode, nonstop_mode, motor_run_samples[MOTOR_SAMPLES];
+uint64_t run_progress, current_millis;
+uint8_t motor_sample_index, sample_tolerance;
+double speed_scale;
 
 
-bool is_motor_running, run_train, boot_motor, motor_run_samples[TRAIN_MOTOR_SAMPLES];
-uint32_t run_progress, motor_speed, stop_motor_boot, current_millis, check_motor_state, motor_reboot_off;
-uint8_t motor_sample_index = 0;
 
-
+/**
+ * Checks whether the motor is actual running or not.
+ */
 bool isMotorRunning() {
 
-  motor_run_samples[motor_sample_index] = digitalRead(MOTOR_SENSE_PIN);
-  motor_sample_index = (motor_sample_index + 1) % TRAIN_MOTOR_SAMPLES;
+  motor_run_samples[motor_sample_index] = digitalRead(MOTOR_PULSE_PIN);
+  motor_sample_index = (motor_sample_index + 1) % MOTOR_SAMPLES;
 
-  uint8_t sample_sum = 0;
-  for (uint8_t i = 0; i < TRAIN_MOTOR_SAMPLES; i++)
-    sample_sum = sample_sum + motor_run_samples[i];
+  uint16_t _sample_sum = 0;
+  for (uint8_t i = 0; i < MOTOR_SAMPLES; i++)
+    _sample_sum = _sample_sum + motor_run_samples[i];
 
-  return (5 < sample_sum && sample_sum < (TRAIN_MOTOR_SAMPLES-5));
+  return (MOTOR_SAMPLE_TOL < _sample_sum && _sample_sum < (MOTOR_SAMPLES-MOTOR_SAMPLE_TOL));
 }
-
 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("SDB Train booting..");
 
-  pinMode(TRAIN_MODE_SW_PIN, INPUT_PULLUP);
+  nonstop_mode = false;
+  speed_scale = 0.0;
+  run_progress = 0;
+  run_train = true;
+  motor_sample_index = 0;
 
-  /** MOTOR SETUP **/
-  Serial.println("Setting up motor..");
-  pinMode(MOTOR_CTRL_PIN, OUTPUT);
+
+  pinMode(TRAIN_MODE_PIN, INPUT_PULLUP);
+  pinMode(MOTOR_PWM_PIN, OUTPUT);
   pinMode(MOTOR_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_SENSE_PIN, INPUT);
-  for (uint8_t i = 0; i < TRAIN_MOTOR_SAMPLES; i++)
-  {
-    isMotorRunning();
-  }
+  pinMode(MOTOR_PULSE_PIN, INPUT);
 
-  Serial.println("Motor ready");
 
-  boot_motor = true;
-  run_progress = millis() % TRAIN_DURATION;
-  run_train = run_progress < TRAIN_RUN_DURATION;
   Serial.println("Setup finished");
 }
 
 
 void loop () {
 
+  // variables to be used throughout the program loop
   current_millis = millis();
   is_motor_running = isMotorRunning();
+  nonstop_mode = digitalRead(TRAIN_MODE_PIN);
 
-  // evaluate if train should run 
-  run_progress = current_millis % TRAIN_DURATION;
-  run_train = run_progress < TRAIN_RUN_DURATION;
+  // compute current run progress
+  run_progress = current_millis % (TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK+TRAIN_T_STOP);
+  run_train = run_progress < (TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK);
 
-  // run train all the time if switched
-  if (digitalRead(TRAIN_MODE_SW_PIN) == true) {
-    run_train = true;
-  }
+  // determine motor mode
+  boot_mode = (run_train && run_progress < TRAIN_T_BOOT && !nonstop_mode);
+  normal_mode = ((run_train && run_progress < ((TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK)-TRAIN_T_BREAK)) || nonstop_mode);
+  breaking_mode = (run_train && run_progress > ((TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK)-TRAIN_T_BREAK) && !nonstop_mode);
+
+  // set motor speed scales
+  // boot motor
+  if (boot_mode) {
+    Serial.print("BOOT");
+    speed_scale = TRAIN_S_BOOT;
+
+
+  // run motor
+  } else if (normal_mode) {
+    Serial.print("RUN");
+    speed_scale = TRAIN_S_MAX;
   
-  // set new time for motor boot to stop
-  if (motor_speed == TRAIN_MIN_SPEED && run_train == true) {
-    stop_motor_boot = current_millis + TRAIN_MOTOR_BOOT_TIME;
-    check_motor_state = current_millis + TRAIN_MOTOR_CHECK_UPSTART;
+
+  // break motor
+  } else if (breaking_mode) {
+    Serial.print("BREAK");
+    int run_progress_break = map(run_progress, (TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK)-TRAIN_T_BREAK, (TRAIN_T_BOOT+TRAIN_T_RUN+TRAIN_T_BREAK),  TRAIN_T_BREAK, 0);
+    speed_scale = ((double)run_progress_break / (double)TRAIN_T_BREAK) * TRAIN_S_MAX;
+
+
+  // motor stop
+  } else {
+    Serial.print("STOP");
+    speed_scale = TRAIN_S_MIN;
   }
-  
-  // determine motor speed
-  boot_motor = (current_millis < stop_motor_boot);
-  motor_speed = (run_train == true) ? TRAIN_MAX_SPEED : TRAIN_MIN_SPEED;
-  //motor_speed = (boot_motor == true) ? TRAIN_BOOT_SPEED : motor_speed;
-
-/*
-  // time to check if motor is running
-  if (run_train == true && current_millis > check_motor_state) {
-    
-    // validate if motor is running
-    if (is_motor_running == false) {
-      motor_speed = TRAIN_MIN_SPEED;
-    } else {
-      // set next check
-      check_motor_state = current_millis + TRAIN_MOTOR_CHECK_UPSTART;
-    }
-  }
-  */
 
 
-  // output to motor
+  // set outputs
   digitalWrite(MOTOR_DIR_PIN, HIGH);
-  //analogWrite(MOTOR_CTRL_PIN, motor_speed);
+  //analogWrite(MOTOR_PWM_PIN, (int)(255 * speed_scale));
   digitalWrite(LED_BUILTIN, run_train);
-  digitalWrite(RPI_ACTIVATE_MUSIC_PIN, run_train);
 
 
-  Serial.print(run_train ? "KØR" : "STOP");
   Serial.print("\t");
-  Serial.print(current_millis);
+  Serial.print((uint32_t)current_millis);
   Serial.print("\t");
-  Serial.print(stop_motor_boot);
+  Serial.print((uint32_t)run_progress);
   Serial.print("\t");
-  Serial.print(run_progress);
-  Serial.print("\t");
-  Serial.print(motor_speed);
-  Serial.print("\t");
-  Serial.print(is_motor_running);
+  Serial.print(speed_scale);
   Serial.println();
 
 }
